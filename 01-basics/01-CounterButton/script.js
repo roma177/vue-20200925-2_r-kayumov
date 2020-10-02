2c797baca23e68367b4ccc0af575b94b9b0533db
Roman K <roman.kaumov@gmail.com>
2020-10-02 13:15:01 +0300

    CounterButton

--- 01-basics/01-CounterButton/script.js
+++ 01-basics/01-CounterButton/script.js
@@ -1,4 +1,14 @@
 import Vue from './vue.esm.browser.js';
 
-// const app = ...
-// Рекомендуется использовать МЕТОД в качестве обработчика события
+const app = new Vue({
+        el: '#app',
+        data: {
+            countedNumber: 0,
+        },
+        methods: {
+            addOne: function() {
+                this.countedNumber += 1
+            }
+        }
+    })
+    // Рекомендуется использовать МЕТОД в качестве обработчика события

5953125571543d9b6cb09b0df0af3d311ad4dd26
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-10-26 16:20:54 +0300

    Added task 404 - NotFound

---
+++ 04-SPA/03-NotFound/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <header>
+      <nav>
+        <ul>
+          <li><router-link to="/page-a">/page-a</router-link></li>
+          <li><router-link to="/page-b">/page-b</router-link></li>
+          <li><router-link to="/page-unknown">/page-unknown</router-link></li>
+        </ul>
+      </nav>
+    </header>
+    <main><router-view /></main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created() {
+    this.$router.replace('/page-a');
+  },
+};
+</script>
+
+<style scoped></style>

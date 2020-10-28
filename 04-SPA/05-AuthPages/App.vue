d48cecabb53956c5c01e68adca960d217707c5e6
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-10-28 12:36:41 +0300

    Added task LoginPage и RegisterPage

---
+++ 04-SPA/05-AuthPages/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <header>
+      <nav>
+        <ul>
+          <li><router-link to="/">/</router-link></li>
+          <li><router-link to="/login">/login</router-link></li>
+          <li><router-link to="/register">/register</router-link></li>
+        </ul>
+      </nav>
+    </header>
+    <main class="page page_onboarding"><router-view /></main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created() {
+    this.$router.replace('/');
+  },
+};
+</script>
+
+<style scoped>
+.page.page_onboarding {
+  max-width: 374px;
+  width: 100%;
+  margin: 0 auto;
+}
+</style>
